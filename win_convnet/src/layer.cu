c8e3a22c95845978d21913b3d82f932c0efcc4d3
s271 <serg271@gmail.com>
2014-05-06 21:17:14 +0300

    layer.cu fix

--- win_convnet/src/layer.cu
+++ win_convnet/src/layer.cu
@@ -387,8 +387,8 @@ void WeightLayer::bpropCommon(NVMatrix& v, PASS_TYPE passType) {
 void WeightLayer::updateWeights() {
 
 	bool use_inc_drop = false;
-	if(_type == "conv")
-		use_inc_drop = true;
+	//if(_type == "conv")
+	//	use_inc_drop = true;
 
     _weights.update(use_inc_drop);
     _biases->update(false);
